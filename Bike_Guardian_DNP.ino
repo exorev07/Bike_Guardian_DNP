6ce20803ae65b72a97bab029cd571cde7efba067
Ekansh <ekansharohi1305@gmail.com>
2025-06-01 21:11:23 +0530

    updated the code with better distance logic and enhanced readability

--- Bike_Guardian_DNP.ino
+++ Bike_Guardian_DNP.ino
@@ -1,6 +1,7 @@
 #include <NewPing.h>
 
-#define MAX_DISTANCE 400  // Max distance to check (in cm)
+// HCSR-04 can check up to 400 cm without error
+#define MAX_DISTANCE 400
 
 #define LEDL 8
 #define LEDR 9
@@ -13,40 +14,25 @@ NewPing USRight(6, 7, MAX_DISTANCE);
 
 void setup() {
   Serial.begin(9600);
+
   pinMode(LEDL, OUTPUT);
   pinMode(LEDR, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(REARLED, OUTPUT);
 
-  digitalWrite(BUZZER, HIGH);
-  digitalWrite(LEDL, HIGH);
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(REARLED, HIGH);
-  delay(100);
-  digitalWrite(BUZZER, LOW);
-  digitalWrite(LEDL, LOW);
-  digitalWrite(LEDR, LOW);
-  digitalWrite(REARLED, LOW);
-  delay(100);
-  digitalWrite(BUZZER, HIGH);
-  digitalWrite(LEDL, HIGH);
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(REARLED, HIGH);
-  delay(100);
-  digitalWrite(BUZZER, LOW);
-  digitalWrite(LEDL, LOW);
-  digitalWrite(LEDR, LOW);
-  digitalWrite(REARLED, LOW);
-  delay(100);
-  digitalWrite(BUZZER, HIGH);
-  digitalWrite(LEDL, HIGH);
-  digitalWrite(LEDR, HIGH);
-  digitalWrite(REARLED, HIGH);
-  delay(100);
-  digitalWrite(BUZZER, LOW);
-  digitalWrite(LEDL, LOW);
-  digitalWrite(LEDR, LOW);
-  digitalWrite(REARLED, LOW);
+  // Startup signal
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(BUZZER, HIGH);
+    digitalWrite(LEDL, HIGH);
+    digitalWrite(LEDR, HIGH);
+    digitalWrite(REARLED, HIGH);
+    delay(100);
+    digitalWrite(BUZZER, LOW);
+    digitalWrite(LEDL, LOW);
+    digitalWrite(LEDR, LOW);
+    digitalWrite(REARLED, LOW);
+    delay(100);
+  }
 }
 
 void loop() {
@@ -57,38 +43,41 @@ void loop() {
   handleLED(leftDist, LEDR);
   handleLED(rightDist, LEDL);
 
-  if (leftDist < 15 || midDist < 15 || rightDist < 15) {
-    beepBuzzer(10);
+  if (leftDist < 100 || midDist < 100 || rightDist < 100) {
+    beepBuzzer(10); // buzzer starts beeping rapidly to alert the rider
   }
 
-  if (leftDist < 50 || midDist < 50 || rightDist < 50) {
-    blinkRearLED(100);
+  // for energy efficiency
+  if (leftDist < 400 || midDist < 400 || rightDist < 400) {
+    blinkRearLED(100); // blinks the LED installed behind the rider, to alert the traffic behind
   }
 }
 
+// distance logic    
 void handleLED(int distance, int pin) {
-  if (distance > 40 && distance <= 50) {
+  if (distance <= 100) {
     digitalWrite(pin, HIGH);
-    delay(500);
-    digitalWrite(pin, LOW);
-    delay(500);
-  } else if (distance > 25 && distance <= 40) {
-    digitalWrite(pin, HIGH);
-    delay(250);
-    digitalWrite(pin, LOW);
-    delay(250);
-  } else if (distance > 10 && distance <= 25) {
+  } 
+  else if (distance <= 200) {
     digitalWrite(pin, HIGH);
     delay(100);
     digitalWrite(pin, LOW);
     delay(100);
-  } else if (distance >= 0 && distance <= 10) {
+  } 
+  else if (distance <= 300) {
     digitalWrite(pin, HIGH);
-    delay(25);
+    delay(500);
     digitalWrite(pin, LOW);
-    delay(25);
-  } else {
-    digitalWrite(pin, LOW);  // Turn off if out of range
+    delay(500);
+  } 
+  else if (distance <= 400) {
+    digitalWrite(pin, HIGH);
+    delay(1000);
+    digitalWrite(pin, LOW);
+    delay(1000);
+  } 
+  else {
+    digitalWrite(pin, LOW);
   }
 }
 
